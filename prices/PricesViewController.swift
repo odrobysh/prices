33a46403fed26003e1415c74b0d90da5559918a5
sasha <alex9rob@gmail.com>
2024-10-23 23:05:46 +0300

    ui

--- prices/PricesViewController.swift
+++ prices/PricesViewController.swift
@@ -2,12 +2,24 @@ import UIKit
 
 class PricesViewController: UIViewController {
 
+    @IBOutlet weak var input: UITextField!
+    @IBOutlet weak var symbol: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var time: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .red
+        
+        input.text = "BTC/USD"
+        symbol.text = "Symbol\nBTC/USD"
+        price.text = "Price\n$48.126,333"
+        time.text = "Time\nAug 7, 9:45 AM"
     }
 
-
+    @IBAction func subscribe() {
+        print("subscribe")
+    }
+    
 }
 
