417e4aafdd0006a44174c4f522518d56eb6f92fe
sasha <alex9rob@gmail.com>
2024-10-23 22:40:32 +0300

    renamed vc

--- prices/PricesViewController.swift
+++ prices/PricesViewController.swift
@@ -1,10 +1,11 @@
 import UIKit
 
-class ViewController: UIViewController {
+class PricesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
 
 
